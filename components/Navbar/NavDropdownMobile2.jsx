94ef6d065a104d3d49ba0dd5ea107c25b2591979
arifbillah0156 <arifbillah0156@gmail.com>
2025-12-13 16:07:03 +0600

    'second'

--- components/Navbar/NavDropdownMobile2.jsx
+++ components/Navbar/NavDropdownMobile2.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NavDropdownMobile2 = ({ href, text }) => {
   return (
     <Link href={href}>
-      <span className="block px-4 py-2 text-sm linkDropdownText_M">{text}</span>
+      <span className="block px-4 py-2 text-lg linkDropdownText_M">{text}</span>
     </Link>
   );
 };
