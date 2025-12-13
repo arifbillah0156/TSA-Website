6274539e5952d9a536d713c2cc0d5fac915ca9d6
arifbillah0156 <arifbillah0156@gmail.com>
2025-12-13 16:15:47 +0600

    third

--- components/Navbar/NavDropdownMobile.jsx
+++ components/Navbar/NavDropdownMobile.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NavDropdownMobile = ({ href, text }) => {
   return (
     <Link href={href}>
-      <span className="block px-4 py-2 underline underline-offset-4 text-lg linkDropdownText_M">{text}</span>
+      <span className="block px-4 py-2 underline underline-offset-4 text-md linkDropdownText_M">{text}</span>
     </Link>
   );
 };
--- components/Navbar/NavLinksMobile.jsx
+++ components/Navbar/NavLinksMobile.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NavLinksMobile = ({ href, text, icon }) => {
   return (
     <div className="text-center rounded-md">
-      <Link href={href} className="block w-full p-2 bg-white text-lc rounded-md text-lg underline underline-offset-4">
+      <Link href={href} className="block w-full p-2 bg-white text-lc rounded-md text-md underline underline-offset-4">
         <i className={`${icon} mr-2`}></i>
         <span className="tracking-widest">
           {text}
