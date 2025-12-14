b8c43a5c84f5074804384609c278dda021921dcc
arifbillah0156 <arifbillah0156@gmail.com>
2025-12-14 19:41:24 +0600

    edit15d4ade

--- components/Navbar/NavHead.jsx
+++ components/Navbar/NavHead.jsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import contact from "@/public/gif/contact.gif"
+import email from "@/public/gif/email.gif"
+import fb from "@/public/gif/follow-us.gif"
+import Image from "next/image";
 
 const NavHead = () => {
   return (
     <div className="bg-[#92279e] text-white text-md md:text-lg p-1 md:p-2 md:py-0 tracking-wider text-center md:flex md:justify-center md:gap-10">
       <div className="md:py-1">
         <Link
-          href={"tel:+8801627449269"}
+          href={"tel:+8801865065125"}
           className="font-mono tracking-normal flex justify-center items-center gap-2"
         >
-          <i className="fa-solid fa-phone"></i>{" "}
-          <span>+88 01627-449269</span>
+          <Image src={contact} width={25} height={25} alt="Contact" className="rounded-full" />
+          <span>+88 01865-065125</span>
         </Link>
       </div>
       <div className="md:py-1">
         <Link
-          href={"mailto:ksua1980@gmail.com"}
+          href={"mailto:taqwamadrasa082@gmail.com"}
           className="font-mono tracking-normal flex justify-center items-center gap-2"
         >
-          <i className="fa-solid fa-envelope"></i>{" "}
-          <span>ksua1980@gmail.com</span>
+          <Image src={email} width={25} height={25} alt="Email" className="rounded-full" />
+          <span>taqwamadrasa082@gmail.com</span>
         </Link>
       </div>
       <div className="md:py-1">
@@ -28,7 +32,7 @@ const NavHead = () => {
           className="font-mono tracking-normal flex justify-center items-center gap-2"
           target="_blank"
         >
-          <i className="fa-brands fa-facebook"></i>{" "}
+          <Image src={fb} width={25} height={25} alt="Facebook" className="rounded-full" />
           <span>Taqwa Shishu Academy</span>
         </Link>
       </div>
